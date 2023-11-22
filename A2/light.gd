a784e561f5363d4fffa26594eb9816c70005ae0d
Leahvv21 <leah.vanvalkengoed@gmx.com>
2023-11-22 16:14:42 +0100

    Fill back light

--- A2/light.gd
+++ A2/light.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 @export var Length: int
 var Negative = false 
+var light = true
+
 
 func _ready():
 	Length = Constants.TILE_SIZE * Length
@@ -15,25 +17,30 @@ func _process(_delta):
 		elif $RayCast2D.target_position == Vector2.DOWN * Length: 
 			$RayCast2D.target_position = Vector2.LEFT * Length
 			Negative = true
+			light = false
 		elif $RayCast2D.target_position == Vector2.LEFT * Length: 
 			$RayCast2D.target_position = Vector2.UP * Length
+			light = false
 		elif $RayCast2D.target_position == Vector2.UP * Length: 
 			$RayCast2D.target_position = Vector2.RIGHT * Length
 			Negative = false
+			light = false
 		elif $RayCast2D.target_position == Vector2.RIGHT * Length: 
 			$RayCast2D.target_position = Vector2.DOWN * Length
+			light = false
 		$RayCast2D.force_raycast_update() # Update the `target_position` immediately
-	light_beam()
+	light_beam(light)
+	light = true 
 
-func light_beam():
+func light_beam(light_bool):
 	var tile = $RayCast2D.get_collider()
 	if tile:
-		if tile.has_method("change"):
+		var pos = $RayCast2D.get_collision_point()
+		if tile.has_method("change") && light_bool:
 			if Negative:
-				var pos = $RayCast2D.get_collision_point() + Vector2(-1,-1)
-				tile.change(pos)
-			else:
-				var pos = $RayCast2D.get_collision_point()
-				tile.change(pos)
+				pos = pos + Vector2(-1,-1)
+			tile.change(pos)
+		if tile.has_method("change_back") && !light_bool:
+			tile.change_back()
 		if !tile.has_method("change"):
 			return
--- A2/overlay.gd
+++ A2/overlay.gd
@@ -1,7 +1,14 @@
 extends TileMap
+var array = []
 
 func change(pos):
 	pos = to_local(pos)
 	var cell = local_to_map(pos)
+	array.append(cell)
 	var vector_light = Vector2(1,0)
 	set_cell(0, cell, 1, vector_light)
+
+func change_back():
+	for cell in array: 
+		set_cell(0, cell, 1, Vector2(0,0))
+	array.clear()
