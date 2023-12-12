3e34fae016da4dda19bdc3366d880a399df08069
JesseNieland <45069038+JesseNieland@users.noreply.github.com>
2023-12-12 23:31:21 +0100

    Mirror Placement Fixed
    Mirror Placement has been done similar to Chest Placement, making them separately available.
    
    This allowed us to fix a bug with mirror placement, as it used the global position of the player before. Now it uses the position of the CollisionShape instead, allowing perfect placement.
    
    Mirror placement functionality has been added to Room 3.

--- A2/MirrorPlacements.gd
+++ A2/MirrorPlacements.gd
@@ -1,24 +1,19 @@
-extends TileMap
+extends Area2D
 var interact = false
 var mirror = preload("res://mirror.tscn")
 
 func _process(_delta):
 	if interact && Input.is_action_just_pressed("interact"):
-		if $"../Player".check_mirror():
-			$"../Player".add_mirror(-1)
+		if $"../../Player".check_mirror():
+			$"../../Player".add_mirror(-1)
 			var mirror_instance = mirror.instantiate()
-			mirror_instance.global_position = $"../Player".global_position
+			mirror_instance.global_position = $CollisionShape2D.global_position
 			$"..".add_child(mirror_instance)
-			
-			# Wat doet deze code?
-			if $Area2D/CollisionShape2D: 
-				$Area2D/CollisionShape2D.queue_free()
-			else:
-				$Area2D/CollisionShape2D2.queue_free()
 
-func _on_area_2d_area_entered(_area):
+
+func _on_area_entered(_area):
 	interact = true
 
 
-func _on_area_2d_area_exited(_area):
+func _on_area_exited(_area):
 	interact = false
