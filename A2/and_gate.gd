10658f6db3e021588724ab12dea76f35e833126e
Leahvv21 <leah.vanvalkengoed@gmx.com>
2023-12-20 01:12:42 +0100

    and gate

--- A2/and_gate.gd
+++ A2/and_gate.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
 func _process(delta):
+	$Sprite2D/RayCast2D.force_raycast_update()
+	$Sprite2D/RayCast2D2.force_raycast_update()
+	$Sprite2D/RayCast2D3.force_raycast_update()
 	if !$Sprite2D/RayCast2D.is_colliding() && !$Sprite2D/RayCast2D2.is_colliding() && !$Sprite2D/RayCast2D3.is_colliding():
 		$Sprite2D/Light.Activate = true 
+	else:
+		$Sprite2D/Light.Activate = false
