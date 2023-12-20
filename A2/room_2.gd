8da5c0d745814772a6d4c0fc812dab3f42498d6c
Leahvv21 <leah.vanvalkengoed@gmx.com>
2023-12-20 14:20:07 +0100

    deur animation

--- A2/room_2.gd
+++ A2/room_2.gd
@@ -1,5 +1,9 @@
 extends Node2D
+var animation = true
 
 func _process(_delta):
 	if $Button.Pressed && $Button2.Pressed && $Button3.Pressed: 
 		$Level_change.monitoring = true 
+		if animation:
+			$Level_change/AnimatedSprite2D.play("open")
+			animation = false 
