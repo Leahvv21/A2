1afa94a1162773b79dddef4b0fb3eb58f92e1424
Leahvv21 <leah.vanvalkengoed@gmx.com>
2023-12-20 14:09:28 +0100

    Button animation

--- A2/button.gd
+++ A2/button.gd
@@ -3,7 +3,10 @@ extends Node2D
 @export var Pressed = false 
 
 func _process(_delta):
+	$Sprite2D/RayCast2D.force_raycast_update()
 	if !$Sprite2D/RayCast2D.is_colliding():
 		Pressed = true
+		$AnimationPlayer.play("on")
 	else:
 		Pressed = false 
+		$AnimationPlayer.play("RESET")
